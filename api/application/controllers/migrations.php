bbc0c2918b3f971a54d51a69c1ac0b4c4e08598f
Brian Foo <brian@youaremyjoy.org>
2016-08-02 12:02:44 -0400

    implement data migrations from prod to dev

--- api/application/controllers/migrations.php
+++ api/application/controllers/migrations.php
@@ -12,6 +12,24 @@ class Migrations extends CI_Controller {
     }
   }
 
+  public function data() {
+    if (ENVIRONMENT != 'development') return false;
+
+    $this->load->model('spaces_model');
+    $jsonData = json_decode(file_get_contents('http://light.friendrnd.com/api/spaces/show/default'), TRUE);
+    $space = $this->spaces_model->getEntryByUid('default');
+
+    if ($jsonData && $space && array_key_exists('space', $jsonData)) {
+      $data = array('data' => json_encode($jsonData['space']['data']));
+      $this->spaces_model->updateEntry($space->id, $data);
+      echo json_encode(array("message" => "success", "data" => $data));
+
+    } else {
+      echo json_encode(array("message" => "failure"));
+    }
+
+  }
+
 }
 /* End of file migrations.php */
 /* Location: ./application/controllers/migrations.php */
